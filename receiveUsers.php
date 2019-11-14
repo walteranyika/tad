fd7b520d6e5f9e64cda630da9bbdaa77a1b1b324
walteranyika <walteranyika@gmail.com>
2019-11-14 19:33:31 +0300

    Updated Users

---
+++ receiveUsers.php
@@ -0,0 +1,21 @@
+<?php
+require 'conn.php';
+if (isset($_POST['data'])){
+    $data= $_POST['data'];
+    $dataJson = json_decode($data);
+    foreach ($dataJson as $item){
+        $pid = $item->pid;
+        $fname = $item->fname;
+        $lname = $item->lname;
+        $cnum = $item->cnum;
+        $deptnum = $item->deptnum;
+        $deptname = $item->deptname;
+        $gender = $item->gender;
+        try{
+            $stm = $pdo->prepare('INSERT INTO `staff`(`pid`, `fname`, `lname`, `cnum`, `deptnum`, `deptname`, `gender`) VALUES (?,?,?,?,?,?,?,?)');
+            $stm->execute([$pid, $fname, $lname, $cnum, $deptnum, $deptname, $gender]);
+        }catch (Exception $e){
+
+        }
+    }
+}
---
+++ uploadUsers.php
@@ -0,0 +1,17 @@
+<?php
+require 'conn.php';
+$leo=date("Y-m-d");
+$stm = $pdo->prepare("select * from staff");
+$stm->execute([]);
+$results = $stm->fetchAll();
+$json = json_encode($results);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://66.228.55.80:8000/digisch/tad/receiveUsers.php");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('data'=>$json)));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$server_output = curl_exec($ch);
+curl_close($ch);
+if ($server_output=="OK"){
+    echo "Completed";
+}
